197dd92e61fca79d3384c725cf705a9fecc2cc30
FirmamentumX <3033439590@qq.com>
2025-07-17 20:04:25 +0800

    Add env yml

--- tools/organize.py
+++ tools/organize.py
@@ -23,9 +23,9 @@ def copy_images_and_labels(src_dir, img_dst, label_dst):
 
 def main():
     parser = argparse.ArgumentParser(description='将训练集和验证集整理为 YOLO 数据集结构')
-    parser.add_argument('train_source', help='训练集源目录（包含图片和标签）',default='/home/airhust/zyt/images/FORTRAIN',)
-    parser.add_argument('val_source', help='验证集源目录（包含图片和标签）',default='/home/airhust/zyt/images/FORTEST')
-    parser.add_argument('target_dir', help='输出 YOLO 数据集的根目录',default='/home/airhust/zyt/images/yolo_dataset')
+    parser.add_argument('train_source', help='训练集源目录（包含图片和标签）',default='/home/airhust/zyt/images/sec_train',)
+    parser.add_argument('val_source', help='验证集源目录（包含图片和标签）',default='/home/airhust/zyt/images/FORTRAIN')
+    parser.add_argument('target_dir', help='输出 YOLO 数据集的根目录',default='/home/airhust/zyt/images/sec_dataset')
     args = parser.parse_args()
 
     # 构建目标目录结构
--- yolo/y8train.py
+++ yolo/y8train.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
+# model = YOLO("yolov8n.pt")
+model = YOLO("/home/airhust/zyt/yolo_run/sectrain/weights/best.pt")
 # model = YOLO("./runs/detect/train/weights/best.pt")
-results = model.train(data="craic.yaml", epochs=100, imgsz=640,patience=30, workers=8, batch=128,save_period=3,cache='True', name='y8train')
+results = model.train(data="craic.yaml", epochs=400, imgsz=640,dropout = 0.5, patience=40, workers=8, batch=128,save_period=3,cache='True',project = '/home/airhust/zyt/yolo_run',name='sectrain2',optimizer='AdamW',cls = 5.5, dfl = 1.0, box = 6.5)
